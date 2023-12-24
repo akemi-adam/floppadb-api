2cb5bb9cb37884055508a590ee293a8edffe2b46
mosiah-adam <mosiah.adam@escolar.ifrn.edu.br>
2023-12-24 11:09:44 -0300

    Função de exportar backup

--- app/routes/_app.php
+++ app/routes/_app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Leaf\FS;
+
 // db()->autoConnect();
 
 app()->use(new \App\Middleware\ConfigDatabaseMiddleware);
@@ -37,5 +39,32 @@ app()->post('/statements', function () {
 });
 
 app()->get('/export', function () {
-    //
+    $path = StoragePath('backups/' . request()->get('database') . '.sql');
+
+    $createFile = function (string $path, int $version = 0) use (&$createFile) {
+        if (!file_exists($path)) {
+            return FS::createFile($path);
+        }
+
+        $version += 1;
+
+        $newPath = StoragePath(sprintf("backups/%s_%s.sql", request()->get('database'), $version));
+
+        return $createFile($newPath, $version);
+    };
+
+    $createFile($path);
+
+    try {
+        $dump = new \Ifsnop\Mysqldump\Mysqldump(
+            sprintf("mysql:host=%s;dbname=%s", _env('DB_HOST', 'localhost'), request()->get('database')),
+            _env('DB_USERNAME', 'root'),
+            _env('DB_PASSWORD')
+        );
+        $dump->start($path);
+    } catch (\Exception $e) {
+        return response()->json([ 'error' => 'Failed to generate backup', 'message' => $e->getMessage() ], 500);
+    }
+
+    response()->download($path, request()->get('database'), 200);
 });
